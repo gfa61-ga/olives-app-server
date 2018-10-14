99e1cd37323ad8f4f71bc9a0581b5bc1d8f223b8
George Al <gfa61.ga@gmail.com>
2018-10-14 23:16:15 +0300

    style: Code formatting

--- routes/suppliers.js
+++ routes/suppliers.js
@@ -3,58 +3,78 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/getall', function(req, res) {
-    var db = {};
-    var MongoClient = require('mongodb').MongoClient;
-    MongoClient.connect('mongodb://127.0.0.1', { useNewUrlParser: true }, function(err, client) {
-        db.collection = client.db('olives').collection('suppliers');
-        db.collection.find({}).toArray(function (err, doc) {
-            if (err) {
-                console.log(err); // LogError ????
-            } else {
-               //console.log(doc);
-              res.json(doc);
-            }
-        });
-    });
+  var db = {};
+  var MongoClient = require('mongodb').MongoClient;
+  MongoClient.connect(
+    'mongodb://127.0.0.1',
+    {useNewUrlParser: true},
+    function(err, client) {
+      db.collection = client.db('olives').collection('suppliers');
+      db.collection.find({}).toArray(function (err, doc) {
+          if (err) {
+              console.log(err); // LogError ????
+          } else {
+             //console.log(doc);
+            res.json(doc);
+          }
+      });
+    }
+  );
 });
 
 router.put('/update/:id', function(req, res) {
-    var id = req.params.id;  //parseInt(req.params.id);  *** when _id is an integer
-    var db = {};
-    var MongoClient = require('mongodb').MongoClient;
-    MongoClient.connect('mongodb://127.0.0.1', { useNewUrlParser: true }, function(err, client) {
-        db.collection = client.db('olives').collection('suppliers');
-        db.collection.findOneAndUpdate(
-          {_id: id},
-          {$set: {...req.body}},
-          { returnOriginal: false }, function (err, doc) {
-            if (err) {
-                console.log(err);
-            } else {
-               //console.log(doc);
-              res.status(200).json({"n": doc["lastErrorObject"]["n"], "updatedExisting": doc["lastErrorObject"]["updatedExisting"], "ok": doc["ok"]} ); //res.status(200).json(doc);
-            }
-        });
-    });
+  var id = req.params.id;  //parseInt(req.params.id);  *** when _id is an integer
+  var db = {};
+  var MongoClient = require('mongodb').MongoClient;
+  MongoClient.connect(
+    'mongodb://127.0.0.1',
+    {useNewUrlParser: true},
+    function(err, client) {
+      db.collection = client.db('olives').collection('suppliers');
+      db.collection.findOneAndUpdate(
+        {_id: id},
+        {$set: {...req.body}},
+        {returnOriginal: false},
+        function (err, doc) {
+          if (err) {
+              console.log(err);
+          } else {
+             //console.log(doc);
+            res.status(200).json({
+              "n": doc["lastErrorObject"]["n"],
+              "ok": doc["ok"],
+              "updatedExisting": doc["lastErrorObject"]["updatedExisting"]
+            });  //res.status(200).json(doc);
+          }
+        }
+      );
+    }
+  );
 });
 
 router.post('/add', function(req, res) {
-    var db = {};
-    var MongoClient = require('mongodb').MongoClient;
-//    res.status(200).json(req.body)
+  var db = {};
+  var MongoClient = require('mongodb').MongoClient;
 
-    MongoClient.connect('mongodb://127.0.0.1', { useNewUrlParser: true }, function(err, client) {
-        db.collection = client.db('olives').collection('suppliers');
-        console.log("Add:", req.body);
-        db.collection.insertOne(req.body, function (err, doc) {
-            if (err) {
-                console.log(err);
-            } else {
-               //console.log(doc);
-              res.status(200).json({"_id": doc["ops"][0]["_id"], "n": doc["result"]["n"], "ok": doc["result"]["ok"]});
-            }
-        });
-    });
+  MongoClient.connect(
+    'mongodb://127.0.0.1',
+    {useNewUrlParser: true},
+    function(err, client) {
+      db.collection = client.db('olives').collection('suppliers');
+      db.collection.insertOne(req.body, function (err, doc) {
+        if (err) {
+            console.log(err);
+        } else {
+           //console.log(doc);
+          res.status(200).json({
+            "n": doc["result"]["n"],
+            "ok": doc["result"]["ok"],
+            "_id": doc["ops"][0]["_id"]
+          });
+        }
+      });  //res.status(200).json(req.body)
+    }
+  );
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,12 +6,6 @@ var suppliers = require('./routes/suppliers');
 
 app.use(cors());
 
-/*app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
-*/
-
 /*
   If you are using valid JSON and are POSTing it with Content-Type: application/json,
   then you can use the bodyParser middleware to parse the request body
@@ -25,99 +19,42 @@ app.use(express.json()); // app.use(bodyParser.json());
 
 app.use('/suppliers',  suppliers);
 
-/*
-app.get('/suppliers', function(req, res) {
-    var db = {};
-    var MongoClient = require('mongodb').MongoClient;
-    MongoClient.connect('mongodb://127.0.0.1', { useNewUrlParser: true }, function(err, client) {
-        db.collection = client.db('olives').collection('suppliers');
-        db.collection.find({}).toArray(function (err, doc) {
-            if (err) {
-                console.log(err); // LogError ????
-            } else {
-               //console.log(doc);
-              res.json(doc);
-            }
-        });
-    });
-});
-
-
-app.put('/suppliers/update/:id', function(req, res) {
-    var id = req.params.id;  //parseInt(req.params.id);  *** when _id is an integer
-    var db = {};
-    var MongoClient = require('mongodb').MongoClient;
-    MongoClient.connect('mongodb://127.0.0.1', { useNewUrlParser: true }, function(err, client) {
-        db.collection = client.db('olives').collection('suppliers');
-        db.collection.findOneAndUpdate(
-          {_id: id},
-          {$set: {...req.body}},
-          { returnOriginal: false }, function (err, doc) {
-            if (err) {
-                console.log(err);
-            } else {
-               //console.log(doc);
-              res.status(200).json({"n": doc["lastErrorObject"]["n"], "updatedExisting": doc["lastErrorObject"]["updatedExisting"], "ok": doc["ok"]} ); //res.status(200).json(doc);
-            }
-        });
-    });
-});
-
-app.post('/suppliers/add', function(req, res) {
-    var db = {};
-    var MongoClient = require('mongodb').MongoClient;
-//    res.status(200).json(req.body)
-
-    MongoClient.connect('mongodb://127.0.0.1', { useNewUrlParser: true }, function(err, client) {
-        db.collection = client.db('olives').collection('suppliers');
-        console.log("Add:", req.body);
-        db.collection.insertOne(req.body, function (err, doc) {
-            if (err) {
-                console.log(err);
-            } else {
-               //console.log(doc);
-              res.status(200).json({"_id": doc["ops"][0]["_id"], "n": doc["result"]["n"], "ok": doc["result"]["ok"]});
-            }
-        });
-    });
-});
-
-*/
 
 app.get('/pickups', function(req, res) {
-    var db = {};
-    var MongoClient = require('mongodb').MongoClient;
-    MongoClient.connect('mongodb://127.0.0.1', { useNewUrlParser: true }, function(err, client) {
-        db.collection = client.db('olives').collection('pickups');
-        db.collection.find({}).toArray(function (err, doc) {
-            if (err) {
-                console.LogError(err);
-            } else {
-               //console.log(doc);
-              res.status(200).json(doc);
-            }
-        });
-    });
+  var db = {};
+  var MongoClient = require('mongodb').MongoClient;
+  MongoClient.connect(
+    'mongodb://127.0.0.1',
+    {useNewUrlParser: true},
+    function(err, client) {
+      db.collection = client.db('olives').collection('pickups');
+      db.collection.find({}).toArray(function (err, doc) {
+        if (err) {
+            console.log(err);
+        } else {
+           //console.log(doc);
+          res.status(200).json(doc);
+        }
+      });
+    }
+  );
 });
 
 /*
-app.get(path, callback); // Read item(s)
-app.post(...); // Create a new item
-app.put(...); // Replace an item
 app.patch(...); // Update an item
 app.delete(...); // Delete an item
 */
 
 app.listen(3500);
 
-
 /*************   to RUN the server:
 // npm run dev   ** Whenever we make a change in the code and save,
 //                * if nodemon is running, it'll restart the Node server with the new code
 //               **
 
-
 /*************   This works in windows porwershell:
+
+$response = Invoke-RestMethod 'http://localhost:3500/suppliers/getall' -Method Get
 
 $person = @{
     _id='123'
