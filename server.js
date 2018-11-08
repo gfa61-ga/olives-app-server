edf0dc5bd2ccd0a74b23dc1256a7da087797a167
George Al <gfa61.ga@gmail.com>
2018-11-08 18:23:43 +0200

    fix

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ var http = require('http');
 //var bodyParser = require('body-parser')
 var app = express();
 //var port = 3200;
-var port = parseInt(process.env.PORT) + 0 || 3200;
+var port = parseInt(process.env.PORT) + 1 || 3200;
+console.log('ioPort:', port)
 var server = http.createServer(app);
-var io = socketIO.listen(server,{origins:'http:/localhost:3000'});
+var io = socketIO.listen(server,{origins:'http://localhost:3000'});
 server.listen(port);  // socket.io real-time server
 
 io.on('connection', (socket) => {
